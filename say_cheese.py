a30b2a031b6f3390a2e0d5e68e2bf3997bf01693
Yan Hong <yh@issuu.com>
2018-03-11 16:25:09 +0100

    Always send the latest captured image

--- say_cheese.py
+++ say_cheese.py
@@ -4,6 +4,6 @@ from datetime import datetime
 def capture():
     dt = str(datetime.now())
     dtime = dt[0:4]+dt[5:7]+dt[8:10]+dt[11:13]+dt[14:16]+dt[17:19]
-    call(["fswebcam", "-d", "/dev/video0", "-r", "640x480", "--no-banner", "./%d.jpg" % int(dtime)])
+    call(["fswebcam", "-d", "/dev/video0", "-r", "640x480", "--no-banner", "./images/%d.jpg" % int(dtime)])
 
 capture()
--- send_email.py
+++ send_email.py
@@ -4,6 +4,8 @@ from email.mime.multipart import MIMEMultipart
 from email.MIMEImage import MIMEImage
 from email.mime.text import MIMEText
 import smtplib
+import glob
+import os
 
 # create message object instance
 msg = MIMEMultipart()
@@ -23,8 +25,13 @@ msg['Subject'] = "Someone rings the doorbell"
 # add in the message body
 msg.attach(MIMEText(message, 'plain'))
 
+# find the latest image in folder images
+list_of_files = glob.glob('images/*.jpg') # * means all if need specific format then *.csv
+latest_file = max(list_of_files, key=os.path.getctime)
+print 'Image "%s" will be attached.' % latest_file
+
 # attach image to message body
-msg.attach(MIMEImage(file("images/test.jpg").read()))
+msg.attach(MIMEImage(file(latest_file).read()))
 
 #create server
 server = smtplib.SMTP('smtp.gmail.com: 587')
